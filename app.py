ab344d403cc5a2865ee88ee807756a8ba7e1d48a
Regina Steffen <reginasteffen@live.com>
2024-06-05 18:08:51 -0500

    created home page with madlib form

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask, request, render_template
 from flask_debugtoolbar import DebugToolbarExtension
 
+from stories import story
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secretkey'
+app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
 
-    
+@app.route('/')
+def home_page():
+    '''show madlib form on home page'''
+    prompts = story.prompts
+    return render_template('home.html', prompts=prompts) 
+
+
